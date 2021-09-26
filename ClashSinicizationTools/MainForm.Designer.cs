36e12798eee50d134f2dbab82ed6f8ee5a09cb9c
Boyce Lig <49801599+BoyceLig@users.noreply.github.com>
2021-09-27 00:22:42 +0800

    更新

--- ClashSinicizationTools/MainForm.Designer.cs
+++ ClashSinicizationTools/MainForm.Designer.cs
@@ -30,11 +30,9 @@ namespace ClashSinicizationTools
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.translationScriptPath = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.translationScriptBrowseButton = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
-            this.clashForWindowsPath = new System.Windows.Forms.TextBox();
             this.ClashBrowseButton = new System.Windows.Forms.Button();
             this.autoClashPathButton = new System.Windows.Forms.Button();
             this.autoTranslationButton = new System.Windows.Forms.Button();
@@ -52,22 +50,16 @@ namespace ClashSinicizationTools
             this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
             this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
             this.toolTip = new System.Windows.Forms.ToolTip(this.components);
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.translationScriptText = new System.Windows.Forms.TextBox();
             this.statusStrip = new System.Windows.Forms.StatusStrip();
             this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
             this.toolStripProgressBar = new System.Windows.Forms.ToolStripProgressBar();
             this.label4 = new System.Windows.Forms.Label();
+            this.saveTranslationScriptButton = new System.Windows.Forms.Button();
+            this.translationScriptPath = new System.Windows.Forms.ComboBox();
+            this.clashForWindowsPath = new System.Windows.Forms.ComboBox();
             this.statusStrip.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // translationScriptPath
-            // 
-            this.translationScriptPath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.translationScriptPath.Location = new System.Drawing.Point(86, 6);
-            this.translationScriptPath.Name = "translationScriptPath";
-            this.translationScriptPath.Size = new System.Drawing.Size(529, 23);
-            this.translationScriptPath.TabIndex = 0;
             // 
             // label1
             // 
@@ -83,12 +75,13 @@ namespace ClashSinicizationTools
             // translationScriptBrowseButton
             // 
             this.translationScriptBrowseButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.translationScriptBrowseButton.Location = new System.Drawing.Point(702, 6);
+            this.translationScriptBrowseButton.Location = new System.Drawing.Point(682, 6);
             this.translationScriptBrowseButton.Name = "translationScriptBrowseButton";
             this.translationScriptBrowseButton.Size = new System.Drawing.Size(75, 23);
             this.translationScriptBrowseButton.TabIndex = 2;
             this.translationScriptBrowseButton.Text = "浏览";
             this.translationScriptBrowseButton.UseVisualStyleBackColor = true;
+            this.translationScriptBrowseButton.Click += new System.EventHandler(this.translationScriptBrowseButton_Click);
             // 
             // label2
             // 
@@ -101,29 +94,21 @@ namespace ClashSinicizationTools
             this.label2.TabIndex = 1;
             this.label2.Text = "Clash For Windows 路径：";
             // 
-            // clashForWindowsPath
-            // 
-            this.clashForWindowsPath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.clashForWindowsPath.Location = new System.Drawing.Point(177, 35);
-            this.clashForWindowsPath.Name = "clashForWindowsPath";
-            this.clashForWindowsPath.Size = new System.Drawing.Size(438, 23);
-            this.clashForWindowsPath.TabIndex = 0;
-            // 
             // ClashBrowseButton
             // 
             this.ClashBrowseButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.ClashBrowseButton.Location = new System.Drawing.Point(702, 35);
+            this.ClashBrowseButton.Location = new System.Drawing.Point(682, 35);
             this.ClashBrowseButton.Name = "ClashBrowseButton";
             this.ClashBrowseButton.Size = new System.Drawing.Size(75, 23);
             this.ClashBrowseButton.TabIndex = 3;
             this.ClashBrowseButton.Text = "浏览";
             this.ClashBrowseButton.UseVisualStyleBackColor = true;
+            this.ClashBrowseButton.Click += new System.EventHandler(this.ClashBrowseButton_Click);
             // 
             // autoClashPathButton
             // 
             this.autoClashPathButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.autoClashPathButton.Location = new System.Drawing.Point(783, 35);
+            this.autoClashPathButton.Location = new System.Drawing.Point(763, 35);
             this.autoClashPathButton.Name = "autoClashPathButton";
             this.autoClashPathButton.Size = new System.Drawing.Size(75, 23);
             this.autoClashPathButton.TabIndex = 3;
@@ -133,31 +118,32 @@ namespace ClashSinicizationTools
             // autoTranslationButton
             // 
             this.autoTranslationButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.autoTranslationButton.Location = new System.Drawing.Point(783, 6);
+            this.autoTranslationButton.Location = new System.Drawing.Point(763, 6);
             this.autoTranslationButton.Name = "autoTranslationButton";
             this.autoTranslationButton.Size = new System.Drawing.Size(75, 23);
             this.autoTranslationButton.TabIndex = 3;
             this.autoTranslationButton.Text = "自动检测";
             this.autoTranslationButton.UseVisualStyleBackColor = true;
+            this.autoTranslationButton.Click += new System.EventHandler(this.autoTranslationButton_Click);
             // 
             // logTextBox
             // 
             this.logTextBox.BackColor = System.Drawing.SystemColors.MenuText;
             this.logTextBox.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.logTextBox.ForeColor = System.Drawing.SystemColors.Window;
-            this.logTextBox.Location = new System.Drawing.Point(0, 355);
+            this.logTextBox.Location = new System.Drawing.Point(0, 344);
             this.logTextBox.Multiline = true;
             this.logTextBox.Name = "logTextBox";
             this.logTextBox.ReadOnly = true;
-            this.logTextBox.Size = new System.Drawing.Size(870, 157);
+            this.logTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.logTextBox.Size = new System.Drawing.Size(850, 157);
             this.logTextBox.TabIndex = 4;
-            this.logTextBox.Text = "空";
             // 
             // label3
             // 
             this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(0, 335);
+            this.label3.Location = new System.Drawing.Point(0, 324);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(68, 17);
             this.label3.TabIndex = 5;
@@ -166,7 +152,7 @@ namespace ClashSinicizationTools
             // CleanLogButton
             // 
             this.CleanLogButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.CleanLogButton.Location = new System.Drawing.Point(795, 332);
+            this.CleanLogButton.Location = new System.Drawing.Point(775, 321);
             this.CleanLogButton.Name = "CleanLogButton";
             this.CleanLogButton.Size = new System.Drawing.Size(75, 23);
             this.CleanLogButton.TabIndex = 3;
@@ -222,46 +208,51 @@ namespace ClashSinicizationTools
             // openTranslationFileButton
             // 
             this.openTranslationFileButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.openTranslationFileButton.Location = new System.Drawing.Point(621, 6);
+            this.openTranslationFileButton.Enabled = false;
+            this.openTranslationFileButton.Location = new System.Drawing.Point(540, 6);
             this.openTranslationFileButton.Name = "openTranslationFileButton";
-            this.openTranslationFileButton.Size = new System.Drawing.Size(75, 23);
+            this.openTranslationFileButton.Size = new System.Drawing.Size(136, 23);
             this.openTranslationFileButton.TabIndex = 2;
-            this.openTranslationFileButton.Text = "打开文件";
+            this.openTranslationFileButton.Text = "用外部程序打开文件";
             this.openTranslationFileButton.UseVisualStyleBackColor = true;
+            this.openTranslationFileButton.Click += new System.EventHandler(this.openTranslationFileButton_Click);
             // 
             // openClashBrowseButton
             // 
             this.openClashBrowseButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.openClashBrowseButton.Location = new System.Drawing.Point(621, 35);
+            this.openClashBrowseButton.Enabled = false;
+            this.openClashBrowseButton.Location = new System.Drawing.Point(540, 35);
             this.openClashBrowseButton.Name = "openClashBrowseButton";
-            this.openClashBrowseButton.Size = new System.Drawing.Size(75, 23);
+            this.openClashBrowseButton.Size = new System.Drawing.Size(136, 23);
             this.openClashBrowseButton.TabIndex = 2;
-            this.openClashBrowseButton.Text = "打开目录";
+            this.openClashBrowseButton.Text = "打开当前目录";
             this.openClashBrowseButton.UseVisualStyleBackColor = true;
             // 
             // openFileDialog
             // 
             this.openFileDialog.FileName = "openFileDialog1";
             // 
-            // textBox2
+            // translationScriptText
             // 
-            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            this.translationScriptText.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBox2.Location = new System.Drawing.Point(0, 147);
-            this.textBox2.Multiline = true;
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(870, 185);
-            this.textBox2.TabIndex = 8;
+            this.translationScriptText.Location = new System.Drawing.Point(0, 147);
+            this.translationScriptText.Multiline = true;
+            this.translationScriptText.Name = "translationScriptText";
+            this.translationScriptText.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.translationScriptText.Size = new System.Drawing.Size(850, 174);
+            this.translationScriptText.TabIndex = 8;
+            this.translationScriptText.TextChanged += new System.EventHandler(this.translationScriptText_TextChanged);
             // 
             // statusStrip
             // 
             this.statusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.toolStripStatusLabel1,
             this.toolStripProgressBar});
-            this.statusStrip.Location = new System.Drawing.Point(0, 512);
+            this.statusStrip.Location = new System.Drawing.Point(0, 501);
             this.statusStrip.Name = "statusStrip";
-            this.statusStrip.Size = new System.Drawing.Size(870, 22);
+            this.statusStrip.Size = new System.Drawing.Size(850, 22);
             this.statusStrip.TabIndex = 9;
             this.statusStrip.Text = "statusStrip";
             // 
@@ -285,12 +276,48 @@ namespace ClashSinicizationTools
             this.label4.TabIndex = 6;
             this.label4.Text = "翻译脚本信息";
             // 
+            // saveTranslationScriptButton
+            // 
+            this.saveTranslationScriptButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.saveTranslationScriptButton.Enabled = false;
+            this.saveTranslationScriptButton.Location = new System.Drawing.Point(732, 123);
+            this.saveTranslationScriptButton.Name = "saveTranslationScriptButton";
+            this.saveTranslationScriptButton.Size = new System.Drawing.Size(118, 23);
+            this.saveTranslationScriptButton.TabIndex = 3;
+            this.saveTranslationScriptButton.Text = "保存当前翻译脚本";
+            this.saveTranslationScriptButton.UseVisualStyleBackColor = true;
+            this.saveTranslationScriptButton.Click += new System.EventHandler(this.saveTranslationScriptButton_Click);
+            // 
+            // translationScriptPath
+            // 
+            this.translationScriptPath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.translationScriptPath.FormattingEnabled = true;
+            this.translationScriptPath.Location = new System.Drawing.Point(86, 6);
+            this.translationScriptPath.Name = "translationScriptPath";
+            this.translationScriptPath.Size = new System.Drawing.Size(448, 25);
+            this.translationScriptPath.TabIndex = 10;
+            this.translationScriptPath.TextUpdate += new System.EventHandler(this.translationScriptPath_TextChanged);
+            // 
+            // clashForWindowsPath
+            // 
+            this.clashForWindowsPath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.clashForWindowsPath.FormattingEnabled = true;
+            this.clashForWindowsPath.Location = new System.Drawing.Point(177, 33);
+            this.clashForWindowsPath.Name = "clashForWindowsPath";
+            this.clashForWindowsPath.Size = new System.Drawing.Size(357, 25);
+            this.clashForWindowsPath.TabIndex = 10;
+            this.clashForWindowsPath.TextUpdate += new System.EventHandler(this.clashForWindowsPath_TextUpdate);
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(870, 534);
-            this.Controls.Add(this.textBox2);
+            this.ClientSize = new System.Drawing.Size(850, 523);
+            this.Controls.Add(this.clashForWindowsPath);
+            this.Controls.Add(this.translationScriptPath);
+            this.Controls.Add(this.translationScriptText);
             this.Controls.Add(this.logTextBox);
             this.Controls.Add(this.statusStrip);
             this.Controls.Add(this.packButton);
@@ -302,6 +329,7 @@ namespace ClashSinicizationTools
             this.Controls.Add(this.label3);
             this.Controls.Add(this.autoTranslationButton);
             this.Controls.Add(this.CleanLogButton);
+            this.Controls.Add(this.saveTranslationScriptButton);
             this.Controls.Add(this.autoClashPathButton);
             this.Controls.Add(this.ClashBrowseButton);
             this.Controls.Add(this.openClashBrowseButton);
@@ -309,8 +337,6 @@ namespace ClashSinicizationTools
             this.Controls.Add(this.translationScriptBrowseButton);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.clashForWindowsPath);
-            this.Controls.Add(this.translationScriptPath);
             this.Name = "MainForm";
             this.Text = "Clash 汉化工具";
             this.Load += new System.EventHandler(this.MainForm_Load);
@@ -322,12 +348,9 @@ namespace ClashSinicizationTools
         }
 
         #endregion
-
-        private System.Windows.Forms.TextBox translationScriptPath;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button translationScriptBrowseButton;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox clashForWindowsPath;
         private System.Windows.Forms.Button ClashBrowseButton;
         private System.Windows.Forms.Button autoClashPathButton;
         private System.Windows.Forms.Button autoTranslationButton;
@@ -345,11 +368,14 @@ namespace ClashSinicizationTools
         private System.Windows.Forms.OpenFileDialog openFileDialog;
         private System.Windows.Forms.SaveFileDialog saveFileDialog;
         private System.Windows.Forms.ToolTip toolTip;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox translationScriptText;
         private System.Windows.Forms.StatusStrip statusStrip;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
         private System.Windows.Forms.ToolStripProgressBar toolStripProgressBar;
+        private System.Windows.Forms.Button saveTranslationScriptButton;
+        private System.Windows.Forms.ComboBox translationScriptPath;
+        private System.Windows.Forms.ComboBox clashForWindowsPath;
     }
 }
 
--- ClashSinicizationTools/MainForm.cs
+++ ClashSinicizationTools/MainForm.cs
@@ -7,11 +7,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Drawing.Text;
+using System.IO;
 
 namespace ClashSinicizationTools
 {
     public partial class MainForm : Form
     {
+        string scriptFilePath, clashPath;
         public MainForm()
         {
             InitializeComponent();
@@ -31,6 +34,116 @@ namespace ClashSinicizationTools
             logTextBox.Text = string.Empty;
         }
 
+        private void openTranslationFileButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        //点击浏览翻译脚本文件查找脚本文件
+        private void translationScriptBrowseButton_Click(object sender, EventArgs e)
+        {
+            openFileDialog.Title = "请选择翻译脚本";
+            openFileDialog.FileName = "英汉对照.txt";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                scriptFilePath = openFileDialog.FileName;
+                translationScriptPath.Text = scriptFilePath;
+                StreamReader streamReader = new StreamReader(scriptFilePath, Encoding.UTF8);
+                translationScriptText.Text = streamReader.ReadToEnd();
+                streamReader.Close();
+            }
+        }
+
+        //保存当前翻译脚本的修改
+        private void saveTranslationScriptButton_Click(object sender, EventArgs e)
+        {
+            StreamWriter streamWriter = new StreamWriter(scriptFilePath, false);
+            streamWriter.WriteLine(translationScriptText.Text);
+            streamWriter.Flush();
+            streamWriter.Close();
+        }
+
+        //翻译脚本地址栏修改时执行
+        private void translationScriptPath_TextChanged(object sender, EventArgs e)
+        {
+            if (translationScriptPath.Text == string.Empty)
+            {
+                //地址栏为空时，打开文件按钮失效
+                openTranslationFileButton.Enabled = false;
+                translationScriptText.Text = string.Empty;
+            }
+            else
+            {
+                //不为空时，打开文件按钮开启
+                openTranslationFileButton.Enabled = true;
+            }
+
+            //地址栏修改时，把地址交给地址变量           
+            if (File.Exists(translationScriptPath.Text))
+            {
+                scriptFilePath = translationScriptPath.Text;
+                translationScriptPath.Text = scriptFilePath;
+                StreamReader streamReader = new StreamReader(scriptFilePath, Encoding.UTF8);
+                translationScriptText.Text = streamReader.ReadToEnd();
+                streamReader.Close();
+            }
+
+
+        }
+
+        //自动检测翻译脚本位置（必须在当前目录）
+        private void autoTranslationButton_Click(object sender, EventArgs e)
+        {
+            scriptFilePath = Directory.GetCurrentDirectory() + @"\" + translationScriptPath.Text;
+            MessageBox.Show(scriptFilePath);
+        }
+
+        //点击浏览文件夹并将path传递给clash path
+        private void ClashBrowseButton_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog.Description = "请选择 Clash for Windows 根路径";
+            if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+            {
+                clashPath = folderBrowserDialog.SelectedPath;
+                clashForWindowsPath.Text = clashPath;
+                for (int i = 0; i < clashForWindowsPath.Items.Count; i++)
+                {
+                    if (clashForWindowsPath.Text != clashForWindowsPath.Items[i].ToString())
+                    {
+                        clashForWindowsPath.Items.Add(clashForWindowsPath.Text);
+                    }
+                }
+            }
+
+        }
+
+        //翻译脚本显示器修改时，检测与源文件匹配，控制保存开关
+        private void translationScriptText_TextChanged(object sender, EventArgs e)
+        {
+            StreamReader streamReader = new StreamReader(scriptFilePath, Encoding.UTF8);
+            if (translationScriptText.Text == streamReader.ReadToEnd())
+            {
+                saveTranslationScriptButton.Enabled = false;
+            }
+            else
+            {
+                saveTranslationScriptButton.Enabled = true;
+            }
+            streamReader.Close();
+        }
+
+        //手动输入path
+        private void clashForWindowsPath_TextUpdate(object sender, EventArgs e)
+        {
+            if (clashForWindowsPath.Text == string.Empty)
+            {
+                openClashBrowseButton.Enabled = false;
+            }
+            else
+            {
+                openClashBrowseButton.Enabled = true;
+            }
+        }
 
     }
 }
