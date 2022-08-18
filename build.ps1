e2581db46009891d90a8cbdcd8e5e2e45db62ae2
Light <47911535+LightAPIs@users.noreply.github.com>
2022-08-18 13:49:00 +0800

    chore: 更新打包流程

--- build.ps1
+++ build.ps1
@@ -4,14 +4,15 @@ $ErrorActionPreference = 'Stop'
 Write-Host 'dotnet SDK info'
 dotnet --info
 
-$exe = 'Clash Sinicization Tool.exe'
+$exe = @('Clash Sinicization Tool.exe', 'CLI.exe')
 $net_tfm = 'net6.0-windows'
 $dllpatcher_tfm = 'net6.0'
 $configuration = 'Release'
-$output_dir = "$PSScriptRoot\ClashSinicizationTool\bin\$configuration"
+$output_dir = @("$PSScriptRoot\ClashSinicizationTool\bin\$configuration", "$PSScriptRoot\ClashSinicizationToolCLI\bin\$configuration")
 $dllpatcher_dir = "$PSScriptRoot\Build\DotNetDllPathPatcher"
 $dllpatcher_exe = "$dllpatcher_dir\bin\$configuration\$dllpatcher_tfm\DotNetDllPathPatcher.exe"
-$proj_path = "$PSScriptRoot\ClashSinicizationTool\ClashSinicizationTool.csproj"
+$proj_path = @("$PSScriptRoot\ClashSinicizationTool\ClashSinicizationTool.csproj", "$PSScriptRoot\ClashSinicizationToolCLI\ClashSinicizationToolCLI.csproj")
+$dist_path = "$PSScriptRoot\Dist\$configuration"
 
 $build    = $buildtfm -eq 'all' -or $buildtfm -eq 'app'
 
@@ -19,16 +20,23 @@ function Build-App
 {
 	Write-Host 'Building .NET App'
 	
-	$outdir = "$output_dir\$net_tfm"
-	$publishDir = "$outdir\publish"
+	for ($i = 0; $i -lt 2; $i++)
+	{
+		$outdir = "{0}\$net_tfm" -f $output_dir[$i]
+		$publishDir = "$outdir\publish"
+		$exe_path = "$publishDir\{0}" -f $exe[$i]
 
-	Remove-Item $publishDir -Recurse -Force -Confirm:$false -ErrorAction Ignore
-	
-	dotnet publish -c $configuration -f $net_tfm $proj_path
-	if ($LASTEXITCODE) { exit $LASTEXITCODE }
+		Remove-Item $publishDir -Recurse -Force -Confirm:$false -ErrorAction Ignore
 
-	& $dllpatcher_exe $publishDir\$exe bin
-	if ($LASTEXITCODE) { exit $LASTEXITCODE }
+		dotnet publish -c $configuration -f $net_tfm $proj_path[$i]
+		if ($LASTEXITCODE) { exit $LASTEXITCODE }
+
+		& $dllpatcher_exe $exe_path bin
+		if ($LASTEXITCODE) { exit $LASTEXITCODE }
+
+		Copy-Item -Path $publishDir\bin\* -Destination $dist_path\bin -Recurse -Force
+		Copy-Item $exe_path -Destination $dist_path
+	}
 }
 
 dotnet build -c $configuration -f $dllpatcher_tfm $dllpatcher_dir\DotNetDllPathPatcher.csproj
@@ -36,8 +44,22 @@ if ($LASTEXITCODE) { exit $LASTEXITCODE }
 
 if ($build)
 {
-    Build-App
-	$outdir = "$output_dir\$net_tfm"
-	$publishDir = "$outdir\publish"
-	Expand-Archive -Path $PSScriptRoot\Node_asar.zip -DestinationPath $publishDir\bin
+	if (Test-Path $PSScriptRoot\Dist)
+	{
+		Remove-Item -Path $PSScriptRoot\Dist -Recurse
+	}
+	if (-not (Test-Path $PSScriptRoot\Dist))
+	{
+		New-Item -Path $PSScriptRoot -Name Dist -ItemType Directory > $null
+	}
+	if (-not (Test-Path $dist_path))
+	{
+		New-Item -Path $PSScriptRoot\Dist -Name $configuration -ItemType Directory > $null
+	}
+	if (-not (Test-Path $dist_path\bin))
+	{
+		New-Item -Path $dist_path -Name bin -ItemType Directory > $null
+	}
+  Build-App
+	Expand-Archive -Path $PSScriptRoot\Node_asar.zip -DestinationPath $dist_path\bin
 }
